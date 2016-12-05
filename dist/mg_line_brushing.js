7fcbe7d9e981e346d0ccffaf966bb7eb87fb8efb
rzeender <robertzeender@gmail.com>
2016-12-05 13:25:16 -0500

    - Debounce touchend/mouseup event to prevent double trigger on mobile

--- dist/mg_line_brushing.js
+++ dist/mg_line_brushing.js
@@ -191,129 +191,134 @@ function brushing() {
     svg.on('touchend', onClickUp);
 
     function onClickUp() {
-        mouseDown = false;
-        svg.classed('mg-brushing-in-progress', false);
+        if(onClickUp['debounce'])
+            clearTimeout(onClickUp['debounce']);
 
-        var xScale = args.scales.X,
-            yScale = args.scales.Y,
-            flatData = [].concat.apply([], args.data),
-            boundedData,
-            yBounds,
-            xBounds,
-            extentX0 = +extentRect.attr('x'),
-            extentX1 = extentX0 + (+extentRect.attr('width')),
-            interval = get_brush_interval(args),
-            offset = 0,
-            mapDtoX = function(d) { return +d[args.x_accessor]; },
-            mapDtoY = function(d) { return +d[args.y_accessor]; };
+        onClickUp['debounce'] = setTimeout(function() {
+            mouseDown = false;
+            svg.classed('mg-brushing-in-progress', false);
 
-        // if we're zooming in: calculate the domain for x and y axes based on the selected rect
-        if (isDragging) {
-            isDragging = false;
+            var xScale = args.scales.X,
+                yScale = args.scales.Y,
+                flatData = [].concat.apply([], args.data),
+                boundedData,
+                yBounds,
+                xBounds,
+                extentX0 = +extentRect.attr('x'),
+                extentX1 = extentX0 + (+extentRect.attr('width')),
+                interval = get_brush_interval(args),
+                offset = 0,
+                mapDtoX = function(d) { return +d[args.x_accessor]; },
+                mapDtoY = function(d) { return +d[args.y_accessor]; };
 
-            if (brushHistory[args.target].brushed) {
-                brushHistory[args.target].steps.push({
-                    max_x: args.brushed_max_x || args.processed.max_x,
-                    min_x: args.brushed_min_x || args.processed.min_x,
-                    max_y: args.brushed_max_y || args.processed.max_y,
-                    min_y: args.brushed_min_y || args.processed.min_y
-                });
-            }
+            // if we're zooming in: calculate the domain for x and y axes based on the selected rect
+            if (isDragging) {
+                isDragging = false;
 
-            brushHistory[args.target].brushed = true;
-
-            boundedData = [];
-            // is there at least one data point in the chosen selection? if not, increase the range until there is.
-            var iterations = 0;
-            while (boundedData.length === 0 && iterations <= flatData.length) {
-
-                var xValX0 = xScale.invert(extentX0);
-                var xValX1 = xScale.invert(extentX1);
-                xValX0 = xValX0 instanceof Date ? xValX0 : interval.round(xValX0);
-                xValX1 = xValX1 instanceof Date ? xValX1 : interval.round(xValX1);
-
-                args.brushed_min_x = xValX0;
-                args.brushed_max_x = Math.max(interval.offset(args.min_x, 1), xValX1);
-
-                boundedData = flatData.filter(function(d) {
-                    var val = d[args.x_accessor];
-                    return val >= args.brushed_min_x && val <= args.brushed_max_x;
-                });
-
-                iterations++;
-            }
-
-            xBounds = d3.extent(boundedData, mapDtoX);
-            args.brushed_min_x = +xBounds[0];
-            args.brushed_max_x = +xBounds[1];
-            xScale.domain(xBounds);
-
-            yBounds = d3.extent(boundedData, mapDtoY);
-            // add 10% padding on the y axis for better display
-            // @TODO: make this an option
-            args.brushed_min_y = yBounds[0] * 0.9;
-            args.brushed_max_y = yBounds[1] * 1.1;
-            yScale.domain(yBounds);
-        }
-        // zooming out on click, maintaining the step history
-        else {
-            if (brushHistory[args.target].brushed) {
-                var previousBrush = brushHistory[args.target].steps.pop();
-                if (previousBrush) {
-                    args.brushed_max_x = previousBrush.max_x;
-                    args.brushed_min_x = previousBrush.min_x;
-                    args.brushed_max_y = previousBrush.max_y;
-                    args.brushed_min_y = previousBrush.min_y;
-
-                    xBounds = [args.brushed_min_x, args.brushed_max_x];
-                    yBounds = [args.brushed_min_y, args.brushed_max_y];
-                    xScale.domain(xBounds);
-                    yScale.domain(yBounds);
-                } else {
-                    brushHistory[args.target].brushed = false;
-
-                    delete args.brushed_max_x;
-                    delete args.brushed_min_x;
-                    delete args.brushed_max_y;
-                    delete args.brushed_min_y;
-
-                    xBounds = [
-                        brushHistory[args.target].original.min_x,
-                        brushHistory[args.target].original.max_x
-                    ];
-
-                    yBounds = [
-                        brushHistory[args.target].original.min_y,
-                        brushHistory[args.target].original.max_y
-                    ];
+                if (brushHistory[args.target].brushed) {
+                    brushHistory[args.target].steps.push({
+                        max_x: args.brushed_max_x || args.processed.max_x,
+                        min_x: args.brushed_min_x || args.processed.min_x,
+                        max_y: args.brushed_max_y || args.processed.max_y,
+                        min_y: args.brushed_min_y || args.processed.min_y
+                    });
                 }
+
+                brushHistory[args.target].brushed = true;
+
+                boundedData = [];
+                // is there at least one data point in the chosen selection? if not, increase the range until there is.
+                var iterations = 0;
+                while (boundedData.length === 0 && iterations <= flatData.length) {
+
+                    var xValX0 = xScale.invert(extentX0);
+                    var xValX1 = xScale.invert(extentX1);
+                    xValX0 = xValX0 instanceof Date ? xValX0 : interval.round(xValX0);
+                    xValX1 = xValX1 instanceof Date ? xValX1 : interval.round(xValX1);
+
+                    args.brushed_min_x = xValX0;
+                    args.brushed_max_x = Math.max(interval.offset(args.min_x, 1), xValX1);
+
+                    boundedData = flatData.filter(function(d) {
+                        var val = d[args.x_accessor];
+                        return val >= args.brushed_min_x && val <= args.brushed_max_x;
+                    });
+
+                    iterations++;
+                }
+
+                xBounds = d3.extent(boundedData, mapDtoX);
+                args.brushed_min_x = +xBounds[0];
+                args.brushed_max_x = +xBounds[1];
+                xScale.domain(xBounds);
+
+                yBounds = d3.extent(boundedData, mapDtoY);
+                // add 10% padding on the y axis for better display
+                // @TODO: make this an option
+                args.brushed_min_y = yBounds[0] * 0.9;
+                args.brushed_max_y = yBounds[1] * 1.1;
+                yScale.domain(yBounds);
             }
-        }
+            // zooming out on click, maintaining the step history
+            else {
+                if (brushHistory[args.target].brushed) {
+                    var previousBrush = brushHistory[args.target].steps.pop();
+                    if (previousBrush) {
+                        args.brushed_max_x = previousBrush.max_x;
+                        args.brushed_min_x = previousBrush.min_x;
+                        args.brushed_max_y = previousBrush.max_y;
+                        args.brushed_min_y = previousBrush.min_y;
 
-        // has anything changed?
-        if (xBounds && yBounds) {
-            if (xBounds[0] < xBounds[1]) {
-                // trigger the brushing callback
+                        xBounds = [args.brushed_min_x, args.brushed_max_x];
+                        yBounds = [args.brushed_min_y, args.brushed_max_y];
+                        xScale.domain(xBounds);
+                        yScale.domain(yBounds);
+                    } else {
+                        brushHistory[args.target].brushed = false;
 
-                var step = {
-                    min_x: xBounds[0],
-                    max_x: xBounds[1],
-                    min_y: yBounds[0],
-                    max_y: yBounds[1]
-                };
+                        delete args.brushed_max_x;
+                        delete args.brushed_min_x;
+                        delete args.brushed_max_y;
+                        delete args.brushed_min_y;
 
-                brushHistory[args.target].current = step;
+                        xBounds = [
+                            brushHistory[args.target].original.min_x,
+                            brushHistory[args.target].original.max_x
+                        ];
 
-                if (args.after_brushing) {
-                    args.after_brushing.apply(this, [step]);
+                        yBounds = [
+                            brushHistory[args.target].original.min_y,
+                            brushHistory[args.target].original.max_y
+                        ];
+                    }
                 }
             }
 
-            // redraw the chart
-            if (!args.brushing_manual_redraw) {
-               MG.data_graphic(args);
+            // has anything changed?
+            if (xBounds && yBounds) {
+                if (xBounds[0] < xBounds[1]) {
+                    // trigger the brushing callback
+
+                    var step = {
+                        min_x: xBounds[0],
+                        max_x: xBounds[1],
+                        min_y: yBounds[0],
+                        max_y: yBounds[1]
+                    };
+
+                    brushHistory[args.target].current = step;
+
+                    if (args.after_brushing) {
+                        args.after_brushing.apply(this, [step]);
+                    }
+                }
+
+                // redraw the chart
+                if (!args.brushing_manual_redraw) {
+                MG.data_graphic(args);
+                }
             }
-        }
+        }, 10);           
     }
 
     return this;
